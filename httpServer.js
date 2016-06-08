8f226a6039cdfb0a66af456aff425d7d213f2a72
lukem512 <lukem95@gmail.com>
2016-06-08 09:57:02 +0100

    Add 404 fallback

--- httpServer.js
+++ httpServer.js
@@ -7,9 +7,19 @@ var bodyParser = require('body-parser');
 
 var SERVER_HTTP_PORT = process.env.SERVER_HTTP_PORT || 8080;
 
-var app = express();
+// Authorization keys to use as request responses
+// These are hard-coded as the values are not currently
+// used for testing
+var AUTH_KEY_JSON = {
+  "SSD": {
+    "AK": 42,
+    "AKID": "everything"
+  }
+};
 
-// Support HTTP variables
+// Instantiate the Express app
+// and support HTTP request variables
+var app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -22,22 +32,23 @@ app.get('/', (req, res) => {
 app.post('/user/login', (req, res) => {
   // Verify that USER and PASSWORD parameters are present
   let user = req.body.USER;
-  if (!user || typeof user == 'undefined') { res.status(400).send('Bad Request'); }
+  if (!user || typeof user == 'undefined') { res.status(400).send('Bad Request'); return; }
 
   let password = req.body.PASSWORD;
-  if (!password || typeof password == 'undefined') { res.status(400).send('Bad Request'); }
+  if (!password || typeof password == 'undefined') { res.status(400).send('Bad Request'); return; }
 
   // Send hard-coded auth. keys
-  res.json({
-    "SSD": {
-      "AK": 42,
-      "AKID": "everything"
-    }});
+  res.json(AUTH_KEY_JSON);
 });
 
 // Update endpoint
 app.post('/Gateway/UpdateDeviceData', (req, res) => {
   res.status(501).send('Not Implemented');
+});
+
+// Fallback to 404
+app.use('*',  (req, res) => {
+  res.status(404).send('Not Found');
 });
 
 // Listen at specified port
